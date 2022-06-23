1d13dd5469f4c77d7fdfc0f8af9d409eab3d5fc5
Yest Izit <hi@dmitry.email>
2022-06-23 14:58:09 +0400

    feat: use randomly generated string in the domain name

--- config.py
+++ config.py
@@ -9,13 +9,13 @@ API_TOKEN = os.environ.get("API_TOKEN")
 CHANNEL_ID = os.environ.get("CHANNEL_ID")
 
 # Get PROJECT_NAME from environment variable
-PROJECT_NAME = os.environ.get("PROJECT_NAME")
+IDENTIFIER = os.environ.get("PROJECT_NAME")
 
 # Get PORT from environment variable
 PORT = os.environ.get("PORT")
 
 HOST = "0.0.0.0"
-WEBHOOK_HOST = f'https://{PROJECT_NAME}.up.railway.app/'  # Enter here your link from Heroku project settings
+WEBHOOK_HOST = f'https://{IDENTIFIER}.up.railway.app/'  # Enter here your link from Heroku project settings
 
 WEBHOOK_URL_PATH = '/webhook/' + API_TOKEN
 WEBHOOK_URL = urljoin(WEBHOOK_HOST, WEBHOOK_URL_PATH)
